d781ded2169b2eb6250de5880ddd211b2bc783af
grasseh <steve.gagnev4si@gmail.com>
2015-08-18 22:55:55 -0400

    More connexion test

--- projects/Troll/anekdotes.php
+++ projects/Troll/anekdotes.php
@@ -1,7 +1,7 @@
 <?php
 try {
     # MS SQL Server and Sybase with PDO_DBLIB
-    $pdo = new PDO("mysql:host=www.grasseh.com;dbname=trollanekdotes,root,Druk1922");
+    $pdo = new PDO("mysql:host=www.grasseh.com;;port=3306;dbname=trollanekdotes,troll,Q6vYLAr6UYafhmZG");
     $cmd = $pdo->prepare("INSERT INTO Users ( name,count ) values ( 'Steve',0 )");
     $pdo->execute();
 
