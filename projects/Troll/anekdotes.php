3009b46b9b0d98addcba8e11250bf687c451ba0b
grasseh <steve.gagnev4si@gmail.com>
2015-08-18 23:09:53 -0400

    More hooks attempt

--- projects/Troll/anekdotes.php
+++ projects/Troll/anekdotes.php
@@ -7,7 +7,7 @@ try {
     $cmd = $pdo->prepare("INSERT INTO Users ( name,count ) values ( 'Steve',0 )");
     $cmd->execute();
     */
-    echo("This page currently works as it shoulds.");
+    echo("This page currently works as it should.");
 
 
 
