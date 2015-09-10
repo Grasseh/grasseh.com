27b07ed65c698371618f457fb0ddbf4ecaa50382
steve.gagne <steve.gagne@anekdotes.com>
2015-09-10 08:54:50 -0400

    Added config example file

---
+++ config.example.php
@@ -0,0 +1,7 @@
+<?php
+    const ABSOLUTE_SERVER_PATH = "grasseh.com";
+    const PROJECT_PATH = "projects/";
+    const CONNEXION_STRING = "mysql:host=www.grasseh.com;port=3306;dbname=Grasseh";
+    const DATABASE_USER = "user";
+    const DATABASE_PASSWORD = "password";
+?>
