cd45bdbb2dee6f705edfa69fbb1128450dec9035
steve.gagne <steve.gagne@anekdotes.com>
2015-09-14 11:54:39 -0400

    Added basic site structure. No design

--- projects/Site/index.php
+++ projects/Site/index.php
@@ -1,10 +1,17 @@
-
 <!DOCTYPE>
 <html>
 <head>
 <title>Under Construction -- Grasseh</title>
 </head>
 <body>
+<?php
+  include("header.php");
+  include("menu.php");
+?>
 <p>This page is under construction. Please come back soon!</p>
+
+<?php
+  include("footer.php");
+?>
 </body>
 </html>
---
+++ projects/Site/menu.php
@@ -0,0 +1,2 @@
+<a href="#">HOME</a>
+<a href="<?php echo(ABSOLUTE_SERVER_PATH);?>/projects">PROJECTS</a>
