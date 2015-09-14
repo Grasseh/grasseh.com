7eae2cceeefe347bd1b7635d158d31c2f77b3189
steve.gagne <steve.gagne@anekdotes.com>
2015-09-14 13:34:14 -0400

    Added Skeleton for grid system

--- projects/Site/index.php
+++ projects/Site/index.php
@@ -2,14 +2,20 @@
 <html>
 <head>
 <title>Under Construction -- Grasseh</title>
+<link rel="stylesheet" href="<?php echo(ABSOLUTE_SERVER_PATH); ?>/public/css/style.css">
+<link rel="stylesheet" href="<?php echo(ABSOLUTE_SERVER_PATH); ?>/public/Skeleton/css/normalize.css">
+<link rel="stylesheet" href="<?php echo(ABSOLUTE_SERVER_PATH); ?>/public/Skeleton/css/skeleton.css">
+<link rel="icon" type="image/png" href="public/favicon.png">
 </head>
 <body>
+
 <?php
   include("header.php");
   include("menu.php");
 ?>
-<p>This page is under construction. Please come back soon!</p>
-
+<div class="content">
+  <p>This page is under construction. Please come back soon!</p>
+</div>
 <?php
   include("footer.php");
 ?>
