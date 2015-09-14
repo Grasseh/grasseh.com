c3ce45ba8b411ab440e37195d95e82af72e932f7
steve.gagne <steve.gagne@anekdotes.com>
2015-09-14 13:59:19 -0400

    Moves Skeleton to libs and added base formatting

--- projects/Site/index.php
+++ projects/Site/index.php
@@ -2,17 +2,25 @@
 <html>
 <head>
 <title>Under Construction -- Grasseh</title>
+<link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="<?php echo(ABSOLUTE_SERVER_PATH); ?>/public/css/style.css">
-<link rel="stylesheet" href="<?php echo(ABSOLUTE_SERVER_PATH); ?>/public/Skeleton/css/normalize.css">
-<link rel="stylesheet" href="<?php echo(ABSOLUTE_SERVER_PATH); ?>/public/Skeleton/css/skeleton.css">
+<link rel="stylesheet" href="<?php echo(ABSOLUTE_SERVER_PATH); ?>/public/libs/Skeleton/css/normalize.css">
+<link rel="stylesheet" href="<?php echo(ABSOLUTE_SERVER_PATH); ?>/public/libs/Skeleton/css/skeleton.css">
 <link rel="icon" type="image/png" href="public/favicon.png">
 </head>
-<body>
-
-<?php
-  include("header.php");
-  include("menu.php");
-?>
+<body class="container">
+<div class="row">
+  <div class="col-2 column">
+  <?php
+    include("header.php");
+  ?>
+  </div>
+  <div class="col-10 column">
+  <?php
+    include("menu.php");
+  ?>
+  </div>
+</div>
 <div class="content">
   <p>This page is under construction. Please come back soon!</p>
 </div>
--- projects/Site/menu.php
+++ projects/Site/menu.php
@@ -1,2 +1,2 @@
 <a href="#">HOME</a>
-<a href="<?php echo(ABSOLUTE_SERVER_PATH);?>/projects">PROJECTS</a>
+<a href="<?php echo(ABSOLUTE_SERVER_PATH);?>/site/projects">PROJECTS</a>
