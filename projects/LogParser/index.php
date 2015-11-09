d8db0d71f175cbb45fa8d6561b787d5d176535fd
Grasseh <steve.gagnev4si@gmail.com>
2015-11-09 09:55:02 -0500

    Added description + page format

--- projects/LogParser/index.php
+++ projects/LogParser/index.php
@@ -9,8 +9,23 @@
 <link rel="icon" type="image/png" href="/public/images/logo.png">
 </head>
 <body class="container">
-  This is my custom log parser. It loads a file and puts it in a readable format.
+<div class="content">
+JSON Log:<br>
+<textarea id="log"></textarea><br>
+<button id="parse">Parse</button>
+</div>
+<div class="dataTablediv">
+</div>
+<hr><hr><hr>
+  <p>
+  This is my custom log parser. It loads a JSON log and puts it in a readable format.<br><br>
 
-  Work in Progress.
+  Work in Progress.<br><br>
+
+  Current file format takes JSONs.<br>
+  JSON Log Example :<br>
+  {"type" : 1, "type_message" : "critical", "date" : "2015-11-15 09:49:12.123 UTC-5", "data" : "2"}<br>
+  Any other value than type, type_message and date will be in the same "other keys" column.<br>
+  </p>
 </body>
 </html>
