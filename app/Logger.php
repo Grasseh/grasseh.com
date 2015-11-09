2e77eb9282d8d8933f9ceba2719bef3d4d6741d1
Grasseh <steve.gagnev4si@gmail.com>
2015-11-09 15:00:01 -0500

    Added logger

---
+++ app/Logger.php
@@ -0,0 +1,32 @@
+<?php
+  class Log{
+    private static function Write($type,$type_message,$data){
+      $date = new DateTime();
+      $object = array("type" => $type,"type_message" => $type_message, "date" => $date->format("Y-m-d H:i:s.u e"));
+      foreach($data as $key=>$value){
+        $object[$key] = $value;
+      }
+      //Additionnal data
+      $object["request_uri"] = $_SERVER["REQUEST_URI"];
+      $object["remote_addr"] = $_SERVER["REMOTE_ADDR"];
+
+      $data = json_encode($object);
+      //Write to file TODO:Make a file per day. Blergh
+      $file = 'logs/log.txt' ;
+      file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+    }
+
+
+    public static function Critical($data){
+      Log::Write(1,"critical",$data);
+    }
+
+    public static function Warn($data){
+      Log::Write(2,"warn",$data);
+    }
+
+    public static function Info($data){
+      Log::Write(3,"info",$data);
+    }
+  }
+?>
