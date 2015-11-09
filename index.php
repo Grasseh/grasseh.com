1b8a8e4dd758bf66a83a355507236e9b580c52e0
Grasseh <steve.gagnev4si@gmail.com>
2015-11-09 09:48:04 -0500

    Added logparser site structure.

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
     include("config.php");
 
 	//Obtenir le chemin de la requête
-	$request = strtolower($_SERVER["REQUEST_URI"]);	
+	$request = strtolower($_SERVER["REQUEST_URI"]);
 	//Retirer la barre oblique à la fin, si nécéssaire
 	if($request[strlen($request)-1] == "/")
 	{
 		$request = substr($request, 0,strlen($request)-1);
-	}	
+	}
 	//Vérifier s'il y a une requête
 	if(strlen($request) > 0)
 	{
@@ -17,13 +17,14 @@
 		if($request[0] == "/")
 		{
 			$request = substr($request, 1);
-		}	
+		}
 		$request = explode("/",$request);
-		
+
 		//Table de routage, selon les projets. Classifiés dans html/projects/...
 		$routes = array(
             "site" => "Site",  //projects/Site
             "troll" => "Troll",  //projects/Troll
+            "logparser" => "LogParser",  //projects/Troll
         );
 		//Vérifier si le tableau existe
 		if(array_key_exists($request[0],$routes))
---
+++ projects/LogParser/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>LogParser -- Grasseh</title>
+<link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="/public/css/style.css">
+<link rel="stylesheet" href="/public/libs/Skeleton/css/normalize.css">
+<link rel="stylesheet" href="/public/libs/Skeleton/css/skeleton.css">
+<link rel="icon" type="image/png" href="/public/images/logo.png">
+</head>
+<body class="container">
+  This is my custom log parser. It loads a file and puts it in a readable format.
+
+  Work in Progress.
+</body>
+</html>
