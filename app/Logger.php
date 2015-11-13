e68d9d1aaa030fb1ac1dd5180d6605134cf84259
Grasseh <steve.gagnev4si@gmail.com>
2015-11-13 17:05:41 -0500

    Upgraded logger with 2 new levels. Logs are now saved by day

--- app/Logger.php
+++ app/Logger.php
@@ -13,22 +13,30 @@
       $object["remote_addr"] = $_SERVER["REMOTE_ADDR"];
 
       $data = json_encode($object);
-      //Write to file TODO:Make a file per day. Blergh
-      $file = 'logs/log.txt' ;
+      //Write to file TODO: Make a folder per month.
+      $file = 'logs/' . $date->format("Ymd") . '.txt' ;
       file_put_contents($file, $data . "\n", FILE_APPEND | LOCK_EX);
     }
 
 
     public static function Critical($data){
-      Log::Write(1,"critical",$data);
+      Log::Write(1,"Critical",$data);
+    }
+
+    public static function Error($data){
+      Log::Write(2,"Error",$data);
     }
 
     public static function Warn($data){
-      Log::Write(2,"warn",$data);
+      Log::Write(3,"Warn",$data);
     }
 
     public static function Info($data){
-      Log::Write(3,"info",$data);
+      Log::Write(4,"Info",$data);
+    }
+
+    public static function Debug($data){
+      Log::Write(5,"Debug",$data);
     }
   }
 ?>
