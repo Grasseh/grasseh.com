17b5d71f7f3c2950ec9d171ac347e284292b5475
Grasseh <steve.gagnev4si@gmail.com>
2015-11-17 14:32:36 -0500

    Added a few meta tags

--- project/Site/footer.php
+++ project/Site/footer.php
@@ -1,3 +1,3 @@
 <div class="footer">
-  <span class="helpFooter">&nbsp;</span>Developped by Grasseh/Steve Gagne 2015 - <?php echo(date("Y",time()));?>
+  <span class="helpFooter">&nbsp;</span>Developped by Grasseh/Steve Gagné 2015 - <?php echo(date("Y",time()));?>
 </div>
--- project/Site/index.php
+++ project/Site/index.php
@@ -1,33 +1,37 @@
 <!DOCTYPE>
 <html>
 <head>
-<title>Under Construction -- Grasseh</title>
-<link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="/public/css/style.css">
-<link rel="stylesheet" href="/public/libs/Skeleton/css/normalize.css">
-<link rel="stylesheet" href="/public/libs/Skeleton/css/skeleton.css">
-<link rel="icon" type="image/png" href="/public/images/logo.png">
-<meta name="google-site-verification" content="5AAUV6kXUaGDfbv0pcyjkZlGkXzefCQJ8-lceyz3eqc" /><body class="container">
-<div class="row top">
-  <div class="col-2 no-spacing-column">
-  <?php
-    include("header.php");
-  ?>
-  </div>
-  <div class="col-10 no-spacing-column">
-  <?php
-    include("menu.php");
-  ?>
-  </div>
+  <title>Grasseh -- Backend programmer</title>
+  <meta charset="utf-8">
+  <link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="/public/css/style.css">
+  <link rel="stylesheet" href="/public/libs/Skeleton/css/normalize.css">
+  <link rel="stylesheet" href="/public/libs/Skeleton/css/skeleton.css">
+  <link rel="icon" type="image/png" href="/public/images/logo.png">
+  <meta name="google-site-verification" content="5AAUV6kXUaGDfbv0pcyjkZlGkXzefCQJ8-lceyz3eqc" />
+  <meta name="description" content="Steve Gagne's personnal website. Contains various projects and a few unique pages for tests and fun." />
+</head>
+<body class="container">
+  <div class="row top">
+    <div class="col-2 no-spacing-column">
+    <?php
+      include("header.php");
+    ?>
+    </div>
+    <div class="col-10 no-spacing-column">
+    <?php
+      include("menu.php");
+    ?>
+    </div>
 
-</div>
-<div class="content">
+  </div>
+  <div class="content">
+    <?php
+      include("routes.php");
+    ?>
+  </div>
   <?php
-    include("routes.php");
+    include("footer.php");
   ?>
-</div>
-<?php
-  include("footer.php");
-?>
 </body>
 </html>
