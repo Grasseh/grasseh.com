4b3ed996dbf6cbf03c5cfce91f488c9fcf4d69a3
Grasseh <steve.gagnev4si@gmail.com>
2015-11-23 21:58:44 -0500

    Added some style to example

--- project/LogParser/index.php
+++ project/LogParser/index.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="/public/css/style.css">
   <link rel="stylesheet" href="/public/libs/Skeleton/css/normalize.css">
   <link rel="stylesheet" href="/public/libs/Skeleton/css/skeleton.css">
+  <link rel="stylesheet" href="/public/LogParser/css/style.css">
   <link rel="icon" type="image/png" href="/public/images/logo.png">
   <meta name="description" content="This is my custom log parser. It loads a JSON log and puts it in a readable format." />
   <script type="text/javascript" src="/public/libs/jquery.js" ></script>
@@ -34,10 +35,13 @@ JSON Log:<br>
   One different JSON Object per log object. There must not be anything encompassing the objects. The reasoning behind this is simple. While you log, you do not want to recreate your entire log file as a new object (or array). That means you simply add a new line containing a new JSON Object.<br><br>
 
   JSON Log Example :<br><br>
+<pre>
+<code>
   {"type" : 1, "type_message" : "critical", "date" : "2015-11-15 09:49:12.123 UTC-5", "data" : "2"}<br>
   {"type" : 4, "type_message" : "warn", "date" : "2015-11-15 19:39:22.323 UTC-5", "data" : "3", "toaster" : "kk"}<br>
   {"type" : 6, "type_message" : "info", "date" : "2015-11-15 20:39:22.323 UTC-5", "data" : "3", "toaster" : "kk", "message" : "User loaded a log."}<br><br>
-
+</code>
+</pre>
 
   Any other value than type, type_message and date will be in the same "other keys" column.<br>
   Each line needs to be its own individual JSON.<br><br>
