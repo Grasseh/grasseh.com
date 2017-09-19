ecb7ca45d250a1e04d3653c39cd183359a80e5d9
Grasseh <steve.gagnev4si@gmail.com>
2017-09-19 09:35:52 -0400

    Add Competitions link + fix css

--- resources/views/site/competitions.blade.php
+++ resources/views/site/competitions.blade.php
@@ -13,18 +13,18 @@
 <hr>
 <div id="competitions" class="competitions">
     <ul>
-        <li>Dreamhack Montreal (ETS Rust) [LoL] -- 13-16th -- 2017</li>
-        <li>Polybash XVII (ETS Blue) [LoL] -- 2nd place -- 2017</li>
-        <li>Lan ETS 2017 (ETS Blue) [LoL] -- 2017</li>
-        <li>Collegiate Star League 2016-2017 (ETS Blue) [LoL] -- 2017</li>
-        <li>Polybash XVI (ETS EsporTS) [CSGO] -- 3rd place -- 2016</li>
-        <li>Lan ETS 2016 (ETS EsporTS) [CSGO] -- 2016</li>
-        <li>Collegiate Star League 2015-2016 (ETS EsporTS) [CSGO] -- 2016</li>
-        <li>Global Gaming (Ken Elite Squad) [CSGO] -- 2nd place -- 2015</li>
-        <li>CFPRLAN (Plant and Defuse) [CSGO] -- 1st place -- 2015</li>
-        <li>CEVO Free S5 (Plant and Defuse) [CSGO] -- 13-5 -- 2014</li>
-        <li>CEVO OPEN S2 (Plant and Defuse) [CSGO] -- 4-4 -- 2013</li>
-        <li>Computer Support CFP Competition  (Solo) -- 3rd place -- 2011</li>
+        <li class="competition">Dreamhack Montreal (ETS Rust) [LoL] -- 13-16th -- 2017</li>
+        <li class="competition">Polybash XVII (ETS Blue) [LoL] -- 2nd place -- 2017</li>
+        <li class="competition">Lan ETS 2017 (ETS Blue) [LoL] -- 2017</li>
+        <li class="competition">Collegiate Star League 2016-2017 (ETS Blue) [LoL] -- 2017</li>
+        <li class="competition">Polybash XVI (ETS EsporTS) [CSGO] -- 3rd place -- 2016</li>
+        <li class="competition">Lan ETS 2016 (ETS EsporTS) [CSGO] -- 2016</li>
+        <li class="competition">Collegiate Star League 2015-2016 (ETS EsporTS) [CSGO] -- 2016</li>
+        <li class="competition">Global Gaming (Ken Elite Squad) [CSGO] -- 2nd place -- 2015</li>
+        <li class="competition">CFPRLAN (Plant and Defuse) [CSGO] -- 1st place -- 2015</li>
+        <li class="competition">CEVO Free S5 (Plant and Defuse) [CSGO] -- 13-5 -- 2014</li>
+        <li class="competition">CEVO OPEN S2 (Plant and Defuse) [CSGO] -- 4-4 -- 2013</li>
+        <li class="competition">Computer Support CFP Competition  (Solo) -- 3rd place -- 2011</li>
     </ul>
 </div>
 @endsection
