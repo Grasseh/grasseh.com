9267086db1d233ea90afe455f493522d2a618afa
Grasseh <steve.gagnev4si@gmail.com>
2017-12-04 14:44:07 -0500

    Add Https Middleware

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,18 @@
+<?php
+
+// https://stackoverflow.com/questions/28402726/laravel-5-redirect-to-https
+namespace MyApp\Http\Middleware;
+
+use Closure;
+
+class HttpsProtocol {
+
+    public function handle($request, Closure $next)
+    {
+            if (!$request->secure() && env('APP_ENV') === 'production') {
+                return redirect()->secure($request->getRequestUri());
+            }
+
+            return $next($request);
+    }
+}
