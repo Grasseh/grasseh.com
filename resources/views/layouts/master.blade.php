14725d50c8093e043ce894d589acc3196d20b1dd
Grasseh <steve@grasseh.com>
2018-01-29 17:41:54 -0500

    Fix double p

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -32,7 +32,7 @@
     @yield('content')
   </div>
     <div class="footer">
-      <span class="helpFooter">&nbsp;</span>Developped by Grasseh/Steve Gagné 2015 - <?php echo(date("Y",time()));?>
+      <span class="helpFooter">&nbsp;</span>Developed by Grasseh/Steve Gagné 2015 - <?php echo(date("Y",time()));?>
     </div>
 </body>
 </html>
