ea54974ad52b6c59d2e24f22558c46ffaf8976d3
Grasseh <steve@grasseh.com>
2018-05-25 11:00:40 -0400

    Added base laravel route logging

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,6 +32,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             //custom
             \App\Http\Middleware\ForceHttps::class,
+            \App\Http\Middleware\LogRequest::class,
         ],
 
         'api' => [
