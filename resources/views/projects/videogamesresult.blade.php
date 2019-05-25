3d3adaa8706c43f28cb8d663be28c1a6e91ada20
Grasseh <steve@grasseh.com>
2019-05-25 16:46:38 -0400

    Add VGR page

---
+++ resources/views/projects/videogamesresult.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.master')
+
+@section('title', 'Video Game Playthrough Archive')
+
+@section('head')
+    @extends('layouts.masterhead')
+@endsection
+
+@section('content')
+<div class="content">
+    <h1> Video Game Playthrough Archive </h1>
+    <h2> Fire Emblem Awakening -- Normal Classic - 2019-03-01 </h2>
+    <ul>
+        <li>Tharja 131</li>
+        <li>Morgan 105</li>
+        <li>Stahl 104</li>
+        <li>Robin 90</li>
+        <li>Gaius 77</li>
+        <li>Noire 71</li>
+        <li>Cordelia 70</li>
+        <li>Chrom 67</li>
+        <li>Cherche 62</li>
+        <li>Vaike 60</li>
+        <li>Severa 54</li>
+        <li>Owain 47</li>
+        <li>Lucina 21</li>
+        <li>Gerome 19</li>
+        <li>Inigo 19</li>
+        <li>Lissa 10</li>
+        <li>Olivia 0</li>
+    </ul>
+</div>
+@endsection
