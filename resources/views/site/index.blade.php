bad539e796f39587d7d227b540d0e181a2117468
Grasseh <steve@grasseh.com>
2019-05-28 15:00:44 -0400

    Add link in home page to vg

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -8,9 +8,9 @@
 
 @section('content')
     <p class="index row">
-         Hello, my name is Steve "Grasseh" Gagné, I am a student in Software Engineering and a TA at <a href="http://www.etsmtl.ca">École de Technologie Supérieure</a>.
+         Hello, my name is Steve "Grasseh" Gagné, I am a bachelor in Software Engineering, graduated from the École de Technologie Supérieure. <a href="https://www.github.com/grasseh/resume/resume_ext.pdf">My résumé can easily be found on Github.</a>
     <br><br>
-    I enjoy playing video games, such as <a href="http://na.op.gg/summoner/userName=grasseh"> League of Legends</a> and <a href="/hall-of-fame">Pokémon</a>. 
+    I enjoy playing <a href="/video-game-result">video games</a>, such as <a href="http://na.op.gg/summoner/userName=grasseh"> League of Legends</a> and <a href="/hall-of-fame">Pokémon</a>. 
     <br><br>
         You might enjoy reading my <a href="/blog">blog</a>, checking my <a href="/projects">personal projects</a>, looking at the <a href="/competitions">competitions</a> I've taken part in or looking through my <a href="http://www.github.com/grasseh">Github repositories.</a>
     <br><br>
