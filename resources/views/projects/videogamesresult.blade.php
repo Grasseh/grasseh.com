17e698e1e6ac42e3fa7b0a4126cd6ebcb24206e6
Grasseh <steve@grasseh.com>
2019-06-14 19:29:44 -0400

    Add Eliwood Run

--- resources/views/projects/videogamesresult.blade.php
+++ resources/views/projects/videogamesresult.blade.php
@@ -48,15 +48,25 @@
         <li>Olivia 0</li>
     </ul>
 
-    <h2 class="smaller"> Fire Emblem Blazing Blade -- Normal Eliwood Mode - 2019-xx-xx </h2>
+    <h2 class="smaller"> Fire Emblem Blazing Blade -- Normal Eliwood Mode - 2019-06-14 </h2>
     <ul>
-        <li>Sain (41)</li>
-        <li>Kent (40)</li>
-        <li>Rath (22)</li>
-        <li>Lyn (21)</li>
-        <li>Lucius (13)</li>
-        <li>Serra (0)</li>
-        <li>Nils+Ninian (0)</li>
+        <li>Format : Unit name (Total kills - Deaths){Lyn Mode kills + Eliwood Mode kills} [Level - MaxHP - Str/Mgc - Skl - Spd - Lck - Def - Res ]
+        <li>Kent          (119){40 + 79}     [Lv 12 - 46 - 17 - 23 - 17 - 06 - 13 - 05]</li>
+        <li>Lucius        (096){13 + 83}     [Lv 15 - 43 - 21 - 23 - 21 - 11 - 06 - 30]</li>
+        <li>Lowen         (095 - 1)          [Lv 15 - 55 - 13 - 16 - 17 - 19 - 22 - 09]</li>
+        <li>Sain          (092){41 + 51}     [Lv 10 - 48 - 22 - 16 - 15 - 14 - 12 - 08]</li>
+        <li>Heath         (087)              [Lv 20 - 54 - 27 - 25 - 23 - 17 - 23 - 12]</li>
+        <li>Rath          (086 - 1){22 + 64} [Lv 17 - 51 - 20 - 24 - 21 - 16 - 12 - 12]</li>
+        <li>Hector        (082)              [Lv 13 - 50 - 26 - 22 - 16 - 14 - 20 - 10]</li>
+        <li>Lyn           (080){21 + 60}     [Lv 17 - 47 - 22 - 29 - 29 - 26 - 09 - 17]</li>
+        <li>Eliwood       (066 - 1)          [Lv 08 - 42 - 19 - 23 - 19 - 18 - 13 - 11]</li>
+        <li>Isadora       (054 - 1)          [Lv 19 - 58 - 19 - 16 - 24 - 16 - 12 - 11]</li>
+        <li>Marcus        (051 - 1)          [Lv 12 - 38 - 19 - 21 - 13 - 12 - 11 - 10]</li>
+        <li>Legault       (041)              [Lv 13 - 42 - 14 - 17 - 24 - 23 - 13 - 12]</li>
+        <li>Priscilla     (022)              [Lv 12 - 32 - 23 - 22 - 18 - 28 - 09 - 24]</li>
+        <li>Serra         (015){00 + 15}     [Lv 09 - 35 - 14 - 15 - 19 - 25 - 07 - 14]</li>
+        <li>Pent          (012)              [Lv 11 - 35 - 19 - 23 - 18 - 16 - 13 - 19]</li>
+        <li>Nils + Ninian (000){00 + 00}     [Lv 20 - 31 - 01 - 00 - 26 - 25 - 12 - 19]</li>
     </ul>
 </div>
     </ul>
