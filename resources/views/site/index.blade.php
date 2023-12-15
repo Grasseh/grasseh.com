a9f12a69e0b2bc347abd14c4d00efbf5da626c1b
Grasseh <steve@grasseh.com>
2023-12-14 19:00:26 -0500

    Update bio

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -8,9 +8,9 @@
 
 @section('content')
     <p class="index row">
-         Hello, my name is Steve "Grasseh" Gagné, I am a bachelor in Software Engineering, graduated from the École de Technologie Supérieure. <a href="https://www.github.com/grasseh/resume/blob/master/resume_ext.pdf">My résumé can easily be found on Github.</a>
+         Hello, my name is Steve "Grasseh" Gagné, I am a bachelor in Software Engineering, currently working as a senior backend Engineer at Nasdaq Metrio.</a>
     <br><br>
-    I enjoy playing <a href="/video-game-results">video games</a>, such as <a href="http://na.op.gg/summoner/userName=grasseh"> League of Legends</a> and <a href="/hall-of-fame">Pokémon</a>. 
+    I enjoy playing <a href="/video-game-results">video games</a>, such as <a href="https://www.fflogs.com/character/id/15234145"> Final Fantasy XIV</a> and <a href="/hall-of-fame">Pokémon</a>. 
     <br><br>
         You might enjoy reading my <a href="/blog">blog</a>, checking my <a href="/projects">personal projects</a>, looking at the <a href="/competitions">competitions</a> I've taken part in or looking through my <a href="http://www.github.com/grasseh">Github repositories.</a>
     <br><br>
